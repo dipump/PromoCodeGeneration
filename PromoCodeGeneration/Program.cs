78c7f44b5c95d4c96a520bc0d7f4e15068d986ee
Lenovo-pc <Lenovo-pc@DESKTOP-HRR0642>
2020-08-10 11:41:39 +0300

    Promotions class created

--- PromoCodeGeneration/Program.cs
+++ PromoCodeGeneration/Program.cs
@@ -27,12 +27,33 @@ namespace PromoCodeGeneration
     // Promotions with the offer price.
     public class PromotionItems
     {
-        public Dictionary<string, int> dictPromotionItems = new Dictionary<string, int>();
-        public decimal Price;
+        public Dictionary<string, int> dictPromotionItems { get; set; }
+        public int Price { get; set; }
     }
 
+    // Active items having promotions.
+    public class Promotions
+    {
+        public List<PromotionItems> lstPromotionItems = new List<PromotionItems>();
+        public Promotions()
+        {
+            PromotionItems A = new PromotionItems();
+            A.dictPromotionItems.Add("A", 3);
+            A.Price = 130;
+            lstPromotionItems.Add(A);
 
-    
+            PromotionItems B = new PromotionItems();
+            B.dictPromotionItems.Add("B", 2);
+            B.Price = 45;
+            lstPromotionItems.Add(B);
+
+            PromotionItems CD = new PromotionItems();
+            CD.dictPromotionItems.Add("C", 1);
+            CD.dictPromotionItems.Add("D", 1);
+            CD.Price = 30;
+            lstPromotionItems.Add(CD);
+        }
+    }
 
 
 }
