bc455afab563394d7e39aad819a9306909353ce4
Lenovo-pc <Lenovo-pc@DESKTOP-HRR0642>
2020-08-18 11:11:11 +0300

    Finished

--- PromoCodeGeneration/Program.cs
+++ PromoCodeGeneration/Program.cs
@@ -29,8 +29,12 @@ namespace PromoCodeGeneration
     // Promotions with the offer price.
     public class PromotionItems
     {
-        public Dictionary<string, int> dictPromotionItems { get; set; }
-        public int Price { get; set; }
+        //public Dictionary<string, int> dictPromotionItems { get; set; }
+        //public int Price { get; set; }
+
+        public Dictionary<string, int> dictPromotionItems = new Dictionary<string, int>();
+        public int Price;
+
     }
 
     // Active items having promotions.
@@ -69,29 +73,72 @@ namespace PromoCodeGeneration
 
             foreach (var promotion in promotions.lstPromotionItems)
             {
-                int offerPrice = 0;
-
-                var key = promotion.dictPromotionItems.First().Key;
-                if (dictOrder.ContainsKey(key))
-                {
-                    int orderQty = dictOrder[key];
-                    int offerQty = promotion.dictPromotionItems[key];
-                    int offerCount = orderQty / offerQty;
-                    int remainingQty = orderQty % offerQty;
-                    dictOrder[key] = remainingQty;
-                    offerPrice = offerCount * promotion.Price;
-                }
-
-                promotionOfferPrice = promotionOfferPrice + offerPrice;
+                var offer = GeneratePromotion(promotion, dictOrder);
+                promotionOfferPrice = promotionOfferPrice + offer;
             }
 
-            var orderList = new List<string>(dictOrder.Keys);
-            foreach (var order in orderList)
+
+            var lstOrder = new List<string>(dictOrder.Keys);
+            foreach (var order in lstOrder)
             {
                 var price = items.dictSKU[order] * dictOrder[order];
                 totalPrice = totalPrice + price;
+                Console.WriteLine("Offer:{0} => {1}", price, totalPrice);
             }
+            Console.WriteLine("Offer:{0}", totalPrice);
             return promotionOfferPrice + totalPrice;
+        }
+
+        // Generate promotions.
+        int GeneratePromotion(PromotionItems promotionItems, Dictionary<string, int> dictOrder)
+        {
+
+            if (promotionItems.dictPromotionItems.Count == 1)
+            {
+                // Single item
+                var key = promotionItems.dictPromotionItems.ElementAt(0).Key;
+                if (dictOrder.ContainsKey(key))
+                {
+                    int orderQty = dictOrder[key];
+                    int offerQty = promotionItems.dictPromotionItems[key];
+                    int offersCount = orderQty / offerQty;
+                    int remainingQty = orderQty % offerQty;
+                    dictOrder[key] = remainingQty;
+                    return offersCount * promotionItems.Price;
+                }
+            }
+            else
+            {
+                // Multiple items
+                var key1 = promotionItems.dictPromotionItems.ElementAt(0).Key;
+                var key2 = promotionItems.dictPromotionItems.ElementAt(1).Key; ;
+                if (dictOrder.ContainsKey(key1) && dictOrder.ContainsKey(key2))
+                {
+                    int orderQty1 = dictOrder[key1];
+                    int offerQty1 = promotionItems.dictPromotionItems[key1];
+                    int offersCount1 = orderQty1 / offerQty1;
+                    int remainingQty1 = orderQty1 % offerQty1;
+
+                    int orderQty2 = dictOrder[key2];
+                    int offerQty2 = promotionItems.dictPromotionItems[key2];
+                    int offersCount2 = orderQty2 / offerQty2;
+                    int remainingQty2 = orderQty2 % offerQty2;
+
+                    if (offersCount1 <= offersCount2)
+                    {
+                        dictOrder[key1] = remainingQty1;
+                        dictOrder[key2] = remainingQty2 + (offerQty2 * (offersCount2 - offersCount1));
+                        return offersCount1 * promotionItems.Price;
+                    }
+                    else
+                    {
+                        dictOrder[key1] = remainingQty1 + (offerQty1 * (offersCount1 - offersCount2));
+                        dictOrder[key2] = remainingQty2;
+                        return offersCount2 * promotionItems.Price;
+                    }
+                }
+            }
+            return 0;
         }
     }
 }
--- PromotionTest/PromotionUnitTests.cs
+++ PromotionTest/PromotionUnitTests.cs
@@ -1,13 +1,62 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using PromoCodeGeneration;
+using System.Collections.Generic;
 
 namespace PromotionTest
 {
-    [TestClass]
     public class PromotionUnitTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        BL objPromotion = new BL();
+
+        [Test, TestCaseSource("OrderScenarioA")]
+        public void ScenarioA(Dictionary<string, int> dictOrder, int expectedTotal)
         {
+            int total = objPromotion.GetPromotionOfferPrice(dictOrder);
+            Assert.AreEqual(total, expectedTotal);
         }
+
+        [Test, TestCaseSource("OrderScenarioB")]
+        public void ScenarioB(Dictionary<string, int> dictOrder, int expectedTotal)
+        {
+            int total = objPromotion.GetPromotionOfferPrice(dictOrder);
+            Assert.AreEqual(total, expectedTotal);
+        }
+
+        [Test, TestCaseSource("OrderScenarioC")]
+        public void ScenarioC(Dictionary<string, int> dictOrder, int expectedTotal)
+        {
+            int total = objPromotion.GetPromotionOfferPrice(dictOrder);
+            Assert.AreEqual(total, expectedTotal);
+        }
+
+        [Test, TestCaseSource("OrderAllTogether")]
+        public void AllTogether(Dictionary<string, int> dictOrder, int expectedTotal)
+        {
+            int total = objPromotion.GetPromotionOfferPrice(dictOrder);
+            Assert.AreEqual(total, expectedTotal);
+        }
+
+        static object[] OrderScenarioA =
+        {
+            new object[]{new Dictionary<string, int>(){{"A",1},{"B",1},{"C",1}},100 }
+        };
+
+        static object[] OrderScenarioB =
+        {
+            new object[]{new Dictionary<string, int>(){{"A",5},{"B",5},{"C",1}},370 }
+        };
+
+        static object[] OrderScenarioC =
+        {
+            new object[]{new Dictionary<string, int>(){{"A",3},{"B",5},{"C",1},{"D",1}},280 }
+        };
+
+        static object[] OrderAllTogether =
+        {
+            new object[]{new Dictionary<string, int>(){{"A",1},{"B",1},{"C",1}},100 },
+            new object[]{new Dictionary<string, int>(){{"A",5},{"B",5},{"C",1}},370 },
+            new object[]{new Dictionary<string, int>(){{"A",3},{"B",5},{"C",1},{"D",1}},280 }
+        };
+
     }
 }
